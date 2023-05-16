8f3add79de84418c62ec15a5b79ae5ec261a3e92
grgtr <sakharov.da@phystech.edu>
2023-05-16 11:12:07 +0300

    some animation fixes

--- js/stars.js
+++ js/stars.js
@@ -2,20 +2,24 @@ const fallButton = document.querySelector(".footer__button-fall");
 const fall = document.querySelector(".fall");
 
 fallButton.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    if (window.innerWidth >= 800) {
-        fall.classList.add("fall_open");
-        fall.classList.add("fall_animation");
+    if ((!localStorage.getItem("stars") || localStorage.getItem("stars") === "none")) {
+        evt.preventDefault();
+        if (window.innerWidth >= 800) {
+            fall.classList.add("fall_open");
+            fall.classList.add("fall_animation");
+        }
+        localStorage.setItem("stars", "raining");
+    } else if (localStorage.getItem("stars") === "raining") {
+        evt.preventDefault();
+        fall.classList.remove("fall_open");
+        fall.classList.remove("fall_animation");
+        localStorage.setItem("stars", "none");
     }
+
 })
 
 window.addEventListener('resize', function () {
     if (fall.classList.contains("fall_open")) {
         fall.classList.toggle("fall_animation");
-    }
-})
-fall.addEventListener('click', function () {
-    if (fall.classList.contains("fall_open")) {
-        fall.classList.remove("fall_open");
     }
 })
